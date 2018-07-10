6ef75c9508ca568a037d7f7b00d1fcfb33e320ee
dansandd <dana.sandler@gmail.com>
2018-07-10 19:57:36 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -10,12 +10,12 @@ STUDENT_NAMES = [
 def first_student_by_index
 puts STUDENT_NAMES.first
 end
-first_student_by_index
+
 
 def fourth_student_by_index
 puts STUDENT_NAMES[3]
 end
-fourth_student_by_index
+
 
 def last_student_by_index
 puts STUDENT_NAMES.last
