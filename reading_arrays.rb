dc655d1294d06aa233893966c9b2690bd4a8ba41
dansandd <dana.sandler@gmail.com>
2018-07-10 20:00:27 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,28 +8,28 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-puts STUDENT_NAMES[0]
+return STUDENT_NAMES[0]
 end
 
 
 def fourth_student_by_index
-retrun STUDENT_NAMES[3]
+return STUDENT_NAMES[3]
 end
 
 
 def last_student_by_index
-puts STUDENT_NAMES.last
+return STUDENT_NAMES.last
 end
 
 def first_student_by_method
-puts STUDENT_NAMES.first
+return STUDENT_NAMES.first
 end
 
 def last_student_by_method
-puts STUDENT_NAMES.last
+return STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-puts STUDENT_NAMES[0,3]
+return STUDENT_NAMES[0,3]
 end
 first_second_and_third_students
